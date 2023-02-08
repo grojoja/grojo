99cae849675ca0394be917b8f7ad527842fbf51a
grojoja <112141963+grojoja@users.noreply.github.com>
2023-02-08 13:46:35 -0800

    Add files via upload

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -45,7 +45,7 @@ public class Engine {
         //
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
-
+        char character = input.charAt(0);
         TETile[][] finalWorldFrame = null;
         return finalWorldFrame;
     }
